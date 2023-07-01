af22a8fce46498129940f7edaa0d1f1ffc65e432
LouieLK <wsadmnbqwe@gmail.com>
2023-07-01 15:09:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from optimization.image_editor_zecon import ImageEditor
 from optimization.arguments import get_arguments
-
+import torch
 if __name__ == "__main__":
 
     args = get_arguments()
     image_editor = ImageEditor(args)
     image_editor.edit_image_by_prompt()
+    torch.cuda.empty_cache()
     image_editor.edit_image_by_image()
+    torch.cuda.empty_cache()
     image_editor.edit_image_by_image_prompt()
     image_editor.save_image()
