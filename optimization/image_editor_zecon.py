f86e521ebf37c503d84e13564a99f2818dccf3f7
LouieLK <wsadmnbqwe@gmail.com>
2023-07-23 16:45:46 +0800

    Update image_editor_zecon.py

--- optimization/image_editor_zecon.py
+++ optimization/image_editor_zecon.py
@@ -673,7 +673,7 @@ class ImageEditor:
 
                 if self.args.l_clip_global_patch != 0:
                     vgg_loss = self.vgg_loss_feature_gram(
-                        x_in, self.style_image) * self.args.l_clip_global_patch/250
+                        x_in, self.style_image) * self.args.l_clip_global_patch*20
                     loss = loss + vgg_loss
                     self.metrics_accumulator.update_metric(
                         "vgg_loss_feature_gram : ", vgg_loss.item())
@@ -905,7 +905,7 @@ class ImageEditor:
                 loss = torch.tensor(0)
                 if self.args.l_clip_global_patch != 0:
                     vgg_loss = self.vgg_loss_feature(
-                        x_in, self.style_image) * self.args.l_clip_global_patch/500
+                        x_in, self.style_image) * self.args.l_clip_global_patch/250
                     loss = loss + vgg_loss
                     self.metrics_accumulator.update_metric(
                         "vgg_loss_feature : ", vgg_loss.item())
