3ce42c3e62d658e13d8100d0cdd97ef0eda86124
LouieLK <wsadmnbqwe@gmail.com>
2023-08-10 17:10:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from optimization.image_editor_zecon import ImageEditor
 from optimization.arguments import get_arguments
 import torch
+import pickle
 if __name__ == "__main__":
 
     args = get_arguments()
@@ -11,3 +12,6 @@ if __name__ == "__main__":
     torch.cuda.empty_cache()
     image_editor.edit_image_by_prompt()
     image_editor.save_image()
+    file_path = 'matrix.pickle'
+    with open(file_path, 'wb') as file:
+        pickle.dump(image_editor.matrix, file)
