8c0fc9eef882de50b10b336b4eba94ad3766db1f
LouieLK <wsadmnbqwe@gmail.com>
2023-09-14 23:14:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,18 +31,18 @@ if __name__ == "__main__":
             torch.cuda.empty_cache()
             # image_editor.edit_image_by_prompt()
             image_editor.save_image()
-            matrix["clip_prompt"].append(image_editor.matrix["clip_prompt"][0])
-            matrix["clip_image"].append(image_editor.matrix["clip_image"][0])
-            matrix["clip_gram"].append(image_editor.matrix["clip_gram"][0])
-            matrix["gram_prompt"].append(image_editor.matrix["gram_prompt"][0])
-            matrix["gram_image"].append(image_editor.matrix["gram_image"][0])
-            matrix["gram_gram"].append(image_editor.matrix["gram_gram"][0])
-            matrix["hybrid_prompt"].append(
-                image_editor.matrix["hybrid_prompt"][0])
-            matrix["hybrid_gram"].append(image_editor.matrix["hybrid_gram"][0])
-            matrix["hybrid_image"].append(
-                image_editor.matrix["hybrid_image"][0])
+            # matrix["clip_prompt"].append(image_editor.matrix["clip_prompt"][0])
+            # matrix["clip_image"].append(image_editor.matrix["clip_image"][0])
+            # matrix["clip_gram"].append(image_editor.matrix["clip_gram"][0])
+            # matrix["gram_prompt"].append(image_editor.matrix["gram_prompt"][0])
+            # matrix["gram_image"].append(image_editor.matrix["gram_image"][0])
+            # matrix["gram_gram"].append(image_editor.matrix["gram_gram"][0])
+            # matrix["hybrid_prompt"].append(
+            #     image_editor.matrix["hybrid_prompt"][0])
+            # matrix["hybrid_gram"].append(image_editor.matrix["hybrid_gram"][0])
+            # matrix["hybrid_image"].append(
+            #     image_editor.matrix["hybrid_image"][0])
         index += 1
-    file_path = 'matrix_{}_{}_{}.pickle'.format(style, args.begin, args.end)
-    with open(file_path, 'wb') as file:
-        pickle.dump(matrix, file)
+    # file_path = 'matrix_{}_{}_{}.pickle'.format(style, args.begin, args.end)
+    # with open(file_path, 'wb') as file:
+    #     pickle.dump(matrix, file)
