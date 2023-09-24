0f82eff838dfd6f610349e60e61f597336979c54
LouieLK <wsadmnbqwe@gmail.com>
2023-09-24 21:42:56 +0800

    Update image_editor_zecon.py

--- optimization/image_editor_zecon.py
+++ optimization/image_editor_zecon.py
@@ -456,8 +456,8 @@ class ImageEditor:
 
             filename = Path(self.args.init_image).stem
             visualization_path = visualization_path.with_name(
-                "{}_{}_{}{}".format(filename, self.args.prompt_tgt, "{:0{width}d}".format(
-                    i, width=output_len), visualization_path.suffix)
+                "{}_{}_{}_{}{}".format(filename, self.args.prompt_tgt, "{:0{width}d}".format(
+                    i, width=output_len),self.args.l_gram, visualization_path.suffix)
             )
             # plt.savefig(visualization_path)
             
